ba1a7ad3ca9230371d1a2fefe0ec784f083c32f3
ThanakornSaokum <thanakorn01350@gmail.com>
2024-01-26 22:46:09 +0700

    lab15-1

--- lab15_1.cpp
+++ lab15_1.cpp
@@ -1,9 +1,31 @@
-include <iostream>
+#include <iostream>
+
 using namespace std;
 
-int main(){
+int main()
+{
+    int a = 5;
+    char b = 'A';
+    char c = b;
+    int *x = &a;
+    char *y = &b;
+    int **z = &x;
+    
+    cout << a << " " << b << " " << c << " " << &x << " " << &y << " " << &z << endl;
+    
+    cout << &a << " " << &y << " " << &y << " " << &x << " " << &y << " " << &z << endl;
+    
+     c = 'F';
+    cout << a << " " << c << " " << c << " " << &x << " " << &y << " " << &z << endl;
+    
+     *y = 'W';
+    cout << a << " " << *y << " " << *y << " " << &x << " " << &y << " " << &z << endl;
+    
+     a = 6;
+    cout << a << " " << *y << " " << *y << " " << &x << " " << &y << " " << &z << endl;
+    
+     a = 7;
+    cout << a << " " << *y << " " << *y << " " << &x << " " << &y << " " << &z << endl;
 
-	
-	
-	return 0;
+    return 0;
 }
