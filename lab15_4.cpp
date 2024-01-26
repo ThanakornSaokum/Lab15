ff2faecf0164072e9baa44490a8199ad14f9b29f
ThanakornSaokum <thanakorn01350@gmail.com>
2024-01-26 22:47:36 +0700

    lab15-4

--- lab15_4.cpp
+++ lab15_4.cpp
@@ -17,3 +17,19 @@ int main(){
 	
 	return 0;
 }
+
+void shuffle(int &a,int &b,int &c,int &d){
+
+    int x[4] = {a,b,c,d};
+    for(int i = 0; i < rand()%4; i++){
+        int j = x[i];
+        x[i] = x[i+1];
+        x[i+1] = j;
+        a = x[0];
+        b = x[1];
+        c = x[2];
+        d = x[3];
+        
+    }
+
+}
